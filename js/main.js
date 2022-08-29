bf6e3358506f7643ef5e7ca93b1f2fd76922ce43
Nefedov Sergey <nefedov38@gmail.com>
2022-08-29 17:37:18 +0300

    laptop version

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
+    $(".owl-carousel").owlCarousel({
+      dots:false,
+      margin:48,
+      loop:true
+    });
   });
+
+  /*Video btn*/
+  const videoBtn= document.querySelector("#video-btn");
+  const videoPic=document.querySelector(".video__picture");
+  const videoWrapper=document.querySelector(".video");
+  const video = document.querySelector("#video-file")
+  
+  
+  videoBtn.addEventListener("click",function(){
+    videoPic.classList.add("none");
+    videoWrapper.classList.remove("video-overlay")
+    videoBtn.classList.add("none");
+
+    if(video.paused){
+      video.play()
+    }
+  })
+
+  video.addEventListener("click",function(){
+    if(video.paused){
+      video.play()
+    }else{
+      videoPic.classList.remove("none");
+      videoBtn.classList.remove("none");
+      videoWrapper.classList.add("video-overlay");
+      video.pause();
+    }
+    
+  })
