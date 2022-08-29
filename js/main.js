23caf4dfe7724d2558dbd0cb0f8a91f90e779fe0
Sergey Nefedov <nefedov38@gmail.com>
2022-08-29 23:00:23 +0300

    mobile

--- js/main.js
+++ js/main.js
@@ -1,36 +1,50 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-      dots:false,
-      margin:48,
-      loop:true
-    });
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    dots: false,
+    responsive: {
+      // breakpoint from 0 up
+      0: {
+        items: 1,
+        stagePadding: 90,
+        margin: 32,
+        loop: true,
+      },
+      // breakpoint from 480 up
+      601: {
+        items: 2,
+      },
+      // breakpoint from 768 up
+      1001: {
+        items: 3,
+        margin: 48,
+      },
+    },
   });
+});
 
-  /*Video btn*/
-  const videoBtn= document.querySelector("#video-btn");
-  const videoPic=document.querySelector(".video__picture");
-  const videoWrapper=document.querySelector(".video");
-  const video = document.querySelector("#video-file")
-  
-  
-  videoBtn.addEventListener("click",function(){
-    videoPic.classList.add("none");
-    videoWrapper.classList.remove("video-overlay")
-    videoBtn.classList.add("none");
+/*Video btn*/
+const videoBtn = document.querySelector("#video-btn");
+const videoPic = document.querySelector(".video__picture");
+const videoWrapper = document.querySelector(".video");
+const video = document.querySelector("#video-file");
 
-    if(video.paused){
-      video.play()
-    }
-  })
+videoBtn.addEventListener("click", function () {
+  videoPic.classList.add("none");
+  videoWrapper.classList.remove("video-overlay");
+  videoBtn.classList.add("none");
 
-  video.addEventListener("click",function(){
-    if(video.paused){
-      video.play()
-    }else{
-      videoPic.classList.remove("none");
-      videoBtn.classList.remove("none");
-      videoWrapper.classList.add("video-overlay");
-      video.pause();
-    }
-    
-  })
+  if (video.paused) {
+    video.play();
+  }
+});
+
+video.addEventListener("click", function () {
+  if (video.paused) {
+    video.play();
+  } else {
+    videoPic.classList.remove("none");
+    videoBtn.classList.remove("none");
+    videoWrapper.classList.add("video-overlay");
+    video.pause();
+  }
+});
